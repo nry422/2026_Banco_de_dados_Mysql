5dda57af1b343623c5ceafcc09868d900be64584
nry422 <henrymuller@gmail.com>
2026-05-05 18:04:08 -0300

    Update comandos_aula_05_05.sql

--- comandos_aula_05_05.sql
+++ comandos_aula_05_05.sql
@@ -43,6 +43,73 @@ select nome, descricao
 from produto 
 where nome like "%smartphone%" or  "%notebook%";
 
+--exercicio 02
+
+--01
+select p.nome, p.descricao
+from produto AS p
+inner join estoque as e
+inner join loja as l
+on p.id = e.id_produto
+and l.id = e.id_loja
+where l.cidade like "Sao Paulo";
+
+--02
+
+select p.nome, c.descricao
+from produto AS p
+inner join caracteristica as c
+inner join produto_caracteristica as pc
+on p.id = pc.id_produto
+and c.id = pc.id_caracteristica;
+
+--03
+
+select l.nome, l.cidade, e.quantidade_disponivel
+from loja AS l
+inner join estoque as e
+on l.id = e.id_loja;
+
+--04
+
+select p.nome, p.descricao
+from produto AS p
+inner join estoque as e
+on p.id = e.id_produto;
+where e.quantidade_disponivel > '10';
+
+--05 nao funciona
+
+select p.nome, e.quantidade_disponivel, l.cidade
+from produto AS p
+inner join estoque as e
+inner join loja as l
+on p.id = e.id_produto and l.id = e.id_loja
+where l.cidade like 'Salvador';
+
+
+--06
+
+select p.nome, p.descricao, c.nome
+from produto AS p
+inner join caracteristica as c
+inner join produto_caracteristica as pc
+on p.id = pc.id_produto and c.id = pc.id_caracteristica
+where c.nome like 'Tela HD';
+
+--07
+
+select p.nome, p.tipo, p.preco
+from produto AS p
+inner join estoque as e
+on p.id = e.id_produto
+where p.preco > '1000' and e.quantidade_disponivel > 0;
+
+
+
+
+
+
 
 
 
